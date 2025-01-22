b9c1020b70a8a7dbb264da908b07c29af7cd856f
bitsh1ft3r <yeimik101@icloud.com>
2025-01-22 18:22:58 -0500

    Ethernet: Add basic registers taken from linux kernel source patches.

--- Xenon/Core/RootBus/HostBridge/PCIBridge/ETHERNET/Ethernet.h
+++ Xenon/Core/RootBus/HostBridge/PCIBridge/ETHERNET/Ethernet.h
@@ -1,5 +1,9 @@
 // Copyright 2025 Xenon Emulator Project
 
+//
+// Xenon Fast Ethernet Adapter Emulation
+//
+
 #pragma once
 
 #include <iostream>
@@ -14,6 +18,53 @@ namespace Xe
 		{
 #define ETHERNET_DEV_SIZE	0x80
 
+			// Register Set and offsets.
+			// Taken from Linux kernel patches for the Xbox 360.
+			enum XE_ETH_REGISTERS
+			{
+				TX_CONFIG = 0x00,
+				TX_DESCRIPTOR_BASE = 0x04,
+				TX_DESCRIPTOR_STATUS = 0x0C,
+				RX_CONFIG = 0x10,
+				RX_DESCRIPTOR_BASE = 0x14,
+				INTERRUPT_STATUS = 0x20,
+				INTERRUPT_MASK = 0x24,
+				CONFIG_0 = 0x28,
+				POWER = 0x30,
+				PHY_CONFIG = 0x40,
+				PHY_CONTROL = 0x44,
+				CONFIG_1 = 0x50,
+				RETRY_COUNT = 0x54,
+				MULTICAST_FILTER_CONTROL = 0x60,
+				ADDRESS_0 = 0x62,
+				MULTICAST_HASH = 0x68,
+				MAX_PACKET_SIZE = 0x78,
+				ADDRESS_1 = 0x7A
+			};
+
+			// Xenon Fast Ethernet PCI Device State struct.
+			struct XE_PCI_STATE
+			{
+				u32 txConfigReg;
+				u32 txDescriptorBaseReg;
+				u32 txDescriptorStatusReg;
+				u32 rxConfigReg;
+				u32 rxDescriptorBaseReg;
+				u32 interruptStatusReg;
+				u32 interruptMaskReg;
+				u32 config0Reg;
+				u32 powerReg;
+				u32 phyConfigReg;
+				u32 phyControlReg;
+				u32 config1Reg;
+				u32 retryCountReg;
+				u32 multicastFilterControlReg;
+				u32 address0Reg;
+				u32 multicastHashReg;
+				u32 maxPacketSizeReg;
+				u32 address1Reg;
+			};
+
 			class ETHERNET : public PCIDevice
 			{
 			public:
@@ -24,6 +75,7 @@ namespace Xe
 				void ConfigWrite(u64 writeAddress, u64 data, u8 byteCount) override;
 
 			private:
+				XE_PCI_STATE ethPciState = { 0 };
 			};
 
 		}
