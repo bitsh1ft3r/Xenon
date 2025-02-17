d5b7e6d9c53c602e882a50f99cf8c33d61631769
bitsh1ft3r <yeimik101@icloud.com>
2025-02-16 19:04:22 -0800

    [ODD] Add several definitions, new commands and the IDENTIFY structure.

--- Xenon/Core/RootBus/HostBridge/PCIBridge/ODD/ODD.h
+++ Xenon/Core/RootBus/HostBridge/PCIBridge/ODD/ODD.h
@@ -294,6 +294,13 @@ struct XE_ATAPI_REGISTERS {
     // When written
     u32 commandReg;
   };
+  // Offset 0xA
+  struct {
+    // When Read
+    u32 altStatusReg;
+    // When written
+    u32 devControlReg;
+  };
 
   /* Control Block */
   // Offset 0x0
@@ -302,13 +309,6 @@ struct XE_ATAPI_REGISTERS {
   u32 dmaStatusReg;
   // Offset 0x4
   u32 dmaTableOffsetReg;
-  // Offset 0x6
-  struct {
-    // When Read
-    u32 alternateStatusReg;
-    // When written
-    u32 deviceControlReg;
-  };
 };
 
 //
@@ -323,6 +323,8 @@ struct XE_ATAPI_DEV_STATE {
   DataBuffer dataWriteBuffer;
   // ATAPI Inquiry Data
   XE_ATAPI_INQUIRY_DATA atapiInquiryData = {0};
+  // ATA Identify structure.
+  XE_ATA_IDENTIFY_DATA atapiIdentifyData = {0};
   // SCSI Command Descriptor Block
   XE_CDB scsiCBD = {0};
   // Direct Memroy Access Processing
@@ -346,7 +348,7 @@ private:
   // RAM pointer. Used for DMA.
   RAM *mainMemory;
 
-  // ATAPI Device State
+  // ATAPI Device State.
   XE_ATAPI_DEV_STATE atapiState = {0};
 
   // SCSI Command Processing
@@ -356,5 +358,6 @@ private:
 
   // Misc
   void atapiReset();
+  void atapiIdentifyPacketDeviceCommand();
   void atapiIdentifyCommand();
 };
--- Xenon/Core/RootBus/HostBridge/PCIBridge/SATA.h
+++ Xenon/Core/RootBus/HostBridge/PCIBridge/SATA.h
@@ -5,7 +5,7 @@
 */
 
 //
-// ATAPI Registers Offsets
+// ATA/ATAPI Registers Offsets
 //
 
 // Communication with disk drive controllers is achieved via I/O registers.
@@ -38,6 +38,12 @@
 // Command Reg (Write)
 #define ATAPI_REG_COMMAND 0x7
 
+// In the Xenon ATA Controller ALTStatus/DevControl is at 0xA.
+// Alternate Status Reg (Read)
+#define ATAPI_REG_ALTERNATE_STATUS 0xA
+// Device Control Reg (Write)
+#define ATAPI_REG_DEVICE_CONTROL 0xA
+
 // Registers Offsets from Control Block
 
 // Direct Memory Access Command
@@ -46,10 +52,6 @@
 #define ATAPI_DMA_REG_STATUS 0x2
 // Direct Memory Access Table Offset
 #define ATAPI_DMA_REG_TABLE_OFFSET 0x4
-// Alternate Status Reg (Read)
-#define ATAPI_REG_ALTERNATE_STATUS 0x6
-// Device Control Reg (Write)
-#define ATAPI_REG_DEVICE_CONTROL 0x6
 
 //
 //  DMA Registers Bitmasks
@@ -649,3 +651,157 @@ COMRESET having been issued, or in response to power-up.
 * SNotification
 */
 // This may be unused. Same as above.
+
+/*
+* This structure is returned by the IDENTIFY_DEVICE and IDENTIFY_PACKET_DEVICE commands.
+*/
+
+struct XE_ATA_IDENTIFY_DATA {
+  u16 generalConfiguration;                // word 0
+  u16 numberOfCylinders;                   // word 1
+  u16 reserved1;                           // word 2
+  u16 numberOfHeads;                       // word 3
+  u16 reserved2[2];                        // word 4-5
+  u16 NumberOfSectorsPerTrack;             // word 6
+  u16 reserved3[3];                        // word 7-9
+  u8 serialNumber[20];                     // word 10-19
+  u16 reserved4[3];                        // word 20-22
+  u8 firmwareRevision[8];                  // word 23-26
+  u8 modelNumber[40];                      // word 27-46
+  u16 maximumBlockTransfer : 8;            // word 47
+  u16 reserved5 : 8;
+  u16 reserved6;                           // word 48
+  u16 capabilities;                        // word 49
+  u16 reserved7;                           // word 50
+  u16 reserved8 : 8;                       // word 51
+  u16 pioCycleTimingMode : 8;
+  u16 reserved9;                           // word 52
+  u16 translationFieldsValid : 3;          // word 53
+  u16 reserved10 : 13;
+  u16 numberOfCurrentCylinders;            // word 54
+  u16 numberOfCurrentHeads;                // word 55
+  u16 currentSectorsPerTrack;              // word 56
+  u32 currentSectorCapacity;               // word 57-58
+  u16 currentMultiSectorSetting;           // word 59
+  u32 userAddressableSectors;              // word 60-61
+  u16 singleWordDMASupport : 8;            // word 62
+  u16 singleWordDMAActive : 8;
+  u16 multiWordDMASupport : 8;             // word 63
+  u16 multiWordDMAActive : 8;
+  u16 advancedPIOModes : 8;                // word 64
+  u16 reserved11 : 8;
+  u16 minimumMWXferCycleTime;              // word 65
+  u16 recommendedMWXferCycleTime;          // word 66
+  u16 minimumPIOCycleTime;                 // word 67
+  u16 minimumPIOCycleTimeIORDY;            // word 68
+  u16 reserved12[11];                      // word 69-79
+  u16 majorRevision;                       // word 80
+  u16 minorRevision;                       // word 81
+  union {                                     // word 82
+    struct {
+      u16 SMARTFeatureSupport : 1;
+      u16 securityModeFeatureSupport : 1;
+      u16 removableMediaFeatureSupport : 1;
+      u16 powerManagementFeatureSupport : 1;
+      u16 packetFeatureSupport : 1;
+      u16 writeCacheFeatureSupport : 1;
+      u16 lookAheadFeatureSupport : 1;
+      u16 releaseInterruptSupport : 1;
+      u16 serviceInterruptSupport : 1;
+      u16 deviceResetCommandSupport : 1;
+      u16 hostProtectedAreaFeatureSupport : 1;
+      u16 reserved13 : 1;
+      u16 writeBufferCommandSupport : 1;
+      u16 readBufferCommandSupport : 1;
+      u16 nopCommandSupport : 1;
+      u16 reserved14 : 1;
+    };
+    u16 dataAsu16;
+  } support1;
+  union {                                     // word 83
+    struct {
+      u16 downloadMicrocodeCommandSupport : 1;
+      u16 dmaQueuedCommandSupport : 1;
+      u16 CFAFeatureSupport : 1;
+      u16 advancedPowerManagementFeatureSupport : 1;
+      u16 mediaStatusNotificationFeatureSupport : 1;
+      u16 powerFromStandbyFeatureSupport : 1;
+      u16 setFeaturesAfterPowerUpRequired : 1;
+      u16 addressOffsetReservedAreaBoot : 1;
+      u16 setMaximumCommandSupport : 1;
+      u16 acousticManagementFeatureSupport : 1;
+      u16 lba48BitFeatureSupport : 1;
+      u16 deviceConfigOverlapFeatureSupport : 1;
+      u16 flushCacheCommandSupport : 1;
+      u16 flushCacheExtCommandSupport : 1;
+      u16 reserved15 : 2;
+    };
+    u16 dataAsu16;
+  } support2;
+  union {                                     // word 84
+    u16 dataAsu16;
+  } support3;
+  union {                                     // word 85
+    struct {
+      u16 SMARTFeatureEnabled : 1;
+      u16 securityModeFeatureEnabled : 1;
+      u16 removableMediaFeatureEnabled : 1;
+      u16 powerManagementFeatureEnabled : 1;
+      u16 packetFeatureEnabled : 1;
+      u16 writeCacheFeatureEnabled : 1;
+      u16 lookAheadFeatureEnabled : 1;
+      u16 releaseInterruptEnabled : 1;
+      u16 serviceInterruptEnabled : 1;
+      u16 deviceResetCommandEnabled : 1;
+      u16 hostProtectedAreaFeatureEnabled : 1;
+      u16 reserved16 : 1;
+      u16 writeBufferCommandEnabled : 1;
+      u16 readBufferCommandEnabled : 1;
+      u16 nopCommandEnabled : 1;
+      u16 reserved17 : 1;
+    };
+    u16 dataAsu16;
+  } enabled1;
+  union {                                     // word 86
+    struct {
+      u16 downloadMicrocodeCommandEnabled : 1;
+      u16 dmaQueuedCommandEnabled : 1;
+      u16 CFAFeatureEnabled : 1;
+      u16 ddvancedPowerManagementFeatureEnabled : 1;
+      u16 mediaStatusNotificationFeatureEnabled : 1;
+      u16 powerFromStandbyFeatureEnabled : 1;
+      u16 setFeaturesAfterPowerUpRequired2 : 1;
+      u16 addressOffsetReservedAreaBoot2 : 1;
+      u16 setMaximumCommandEnabled : 1;
+      u16 acousticManagementFeatureEnabled : 1;
+      u16 lba48BitFeatureEnabled : 1;
+      u16 deviceConfigOverlapFeatureEnabled : 1;
+      u16 flushCacheCommandEnabled : 1;
+      u16 flushCacheExtCommandEnabled : 1;
+      u16 reserved18 : 2;
+    };
+    u16 dataAsu16;
+  } enabled2;
+  union {                                     // word 87
+    u16 dataAsu16;
+  } enabled3;
+  u16 ultraDMASupport : 8;                 // word 88
+  u16 ultraDMAActive : 8;
+  u16 reserved19[11];                      // word 89-99
+  u32 userAddressableSectors48Bit[2];       // word 100-104
+  u16 reserved20[23];                      // word 104-126
+  u16 mediaStatusNotification : 2;         // word 127
+  u16 reserved21 : 6;
+  u16 deviceWriteProtect : 1;
+  u16 reserved22 : 7;
+  u16 securitySupported : 1;               // word 128
+  u16 securityEnabled : 1;
+  u16 securityLocked : 1;
+  u16 securityFrozen : 1;
+  u16 securityCountExpired : 1;
+  u16 securityEraseSupported : 1;
+  u16 reserved23 : 2;
+  u16 securityLevel : 1;
+  u16 reserved24 : 7;
+  u16 reserved25[127];                     // word 129-255
+};
